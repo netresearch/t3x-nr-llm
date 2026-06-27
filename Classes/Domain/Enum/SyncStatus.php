b62a27c4b458d39ba0108ec741a4910b4676f5e5
Sebastian Mendel <info@sebastianmendel.de>
2026-06-27 22:08:29 +0200

    feat(skills): add SkillSourceType, SyncStatus, SupportStatus enums
    Signed-off-by: Sebastian Mendel <info@sebastianmendel.de>

---
+++ Classes/Domain/Enum/SyncStatus.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Copyright (c) 2025-2026 Netresearch DTT GmbH
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace Netresearch\NrLlm\Domain\Enum;
+
+enum SyncStatus: string
+{
+    case NEVER_SYNCED = 'never_synced';
+    case SYNCING = 'syncing';
+    case OK = 'ok';
+    case PARTIAL = 'partial';
+    case ERROR = 'error';
+
+    /**
+     * @return list<string>
+     */
+    public static function values(): array
+    {
+        return array_map(static fn(self $c): string => $c->value, self::cases());
+    }
+
+    public static function isValid(string $value): bool
+    {
+        return in_array($value, self::values(), true);
+    }
+
+    public static function tryFromString(string $value): ?self
+    {
+        return self::tryFrom($value);
+    }
+}
